a6913d3dc2d1f44cb06f9d16fc41edbc13303b17
yoldenzeel <y.oldenzeel@flusso.nl>
2026-03-20 22:44:25 +0000

    fix(dependencies): update Cloudflare adapter to use @sveltejs/adapter-cloudflare

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapterCloudflare from '@sveltejs/adapter-cloudflare-workers';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -17,7 +17,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: useWorkerAdapter ? adapterCloudflare() : adapterNode()
+		adapter: useWorkerAdapter
+			? adapterCloudflare({
+					routes: {
+						include: ['/*'],
+						exclude: ['<all>']
+					}
+				})
+			: adapterNode()
 	}
 };
 

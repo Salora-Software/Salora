4ea86f1d52b7a3a0b944d76b3a6d94ae2d3a09a8
yoldenzeel <y.oldenzeel@flusso.nl>
2026-03-30 10:52:33 +0000

    refactor: remove unused database configuration and worker client files

--- packages/database/index.worker.ts
+++
@@ -1,3 +0,0 @@
-export * from "./prisma/generated/prisma-worker/client";
-export { createWorkerClient, createWorkerClient as createClient } from "./src/worker";
-export type { WorkerDatabaseBinding } from "./src/worker";
